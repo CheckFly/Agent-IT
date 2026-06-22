eaa2c8b7657f53388236a20c3025634ecb2b7d9b
TM <tmangeard@gmx.fr>
2026-06-22 16:38:49 +0200

    add_read computer_uuid

--- src/agent_it/client/system.py
+++ src/agent_it/client/system.py
@@ -1,5 +1,24 @@
 import socket
 import time
+import subprocess
+
+
+
+def get_computer_uuid():
+
+    result = subprocess.run(
+        ["wmic", "csproduct", "get", "uuid"],
+        capture_output=True,
+        text=True
+    )
+
+    lines = [
+        line.strip()
+        for line in result.stdout.splitlines()
+        if line.strip()
+    ]
+
+    return lines[1]
 
 
 def get_system_info():
