41e058769d815e263c1186d297d7eec6584006a5
TM <tmangeard@gmx.fr>
2026-06-23 16:09:36 +0200

    add logger on file and explixit error send

--- src/agent_it/client/sender.py
+++ src/agent_it/client/sender.py
@@ -1,21 +1,71 @@
 import requests
-from agent_it.common.config import SERVER_URL
+
+from agent_it.common.config import (
+    SERVER_URL,
+    AGENT_NAME,
+    AGENT_VERSION
+)
+
+from agent_it.common.logger import logger
+
+from agent_it.client.system import (
+    get_hostname,
+    get_computer_uuid,
+    get_ip_address
+)
 
 
 def send_event(event):
 
+    hostname = get_hostname()
+    ip_address = get_ip_address()
+    computer_uuid = get_computer_uuid()
+
+    payload = {
+        "source_application": AGENT_NAME,
+        "agent_version": AGENT_VERSION,
+        "hostname": hostname,
+        "computer_uuid": computer_uuid,
+        "ip_address": ip_address,
+        "event_id": event["event_id"],
+        "record_id": event["record_id"],
+        "event_timestamp": event["event_timestamp"].isoformat()
+    }
+
+    logger.info(
+        f"Envoi EventID={event['event_id']} "
+        f"RecordID={event['record_id']} "
+        f"Host={hostname} "
+        f"IP={ip_address} "
+        f"Vers={SERVER_URL}"
+    )
+
     try:
 
-        r = requests.post(
+        response = requests.post(
             SERVER_URL,
-            json=event,
+            json=payload,
             timeout=5
         )
 
-        if r.status_code == 200:
+        logger.info(
+            f"Réponse serveur : "
+            f"{response.status_code}"
+        )
+
+        if response.status_code == 200:
             return True
 
-    except Exception:
-        pass
+        logger.error(
+            f"Erreur HTTP : "
+            f"{response.status_code} "
+            f"{response.text}"
+        )
+
+    except Exception as e:
+
+        logger.exception(
+            f"Erreur envoi EventID={event['event_id']}"
+        )
 
     return False
--- src/agent_it/common/logger.py
+++ src/agent_it/common/logger.py
@@ -1,8 +1,32 @@
 import logging
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s"
+from agent_it.common.config import (
+    LOG_DIR
 )
 
-logger = logging.getLogger("Agent_IT")
+LOG_FILE = LOG_DIR / "agent_it.log"
+
+logger = logging.getLogger(
+    "Agent_IT"
+)
+
+logger.setLevel(
+    logging.INFO
+)
+
+formatter = logging.Formatter(
+    "%(asctime)s [%(levelname)s] %(message)s"
+)
+
+file_handler = logging.FileHandler(
+    LOG_FILE,
+    encoding="utf-8"
+)
+
+file_handler.setFormatter(
+    formatter
+)
+
+logger.addHandler(
+    file_handler
+)
